ad301a0dc241536943da85b578ffd14865c5f8ff
you06 <you1474600@gmail.com>
2019-10-06 22:17:29 +0800

    add footer
    Signed-off-by: you06 <you1474600@gmail.com>

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>
-    footer
+  <div class="footer-container">
+    <div class="footer-block">
+      <a href="https://github.com/you06/cosmos-blog" target="_blank">
+        <b-icon
+          icon="github-circle"
+          size="is-medium"
+        />
+      </a>
+      <a :href="creativeCommons[cc].url" target="_blank">
+        <img
+          :src="creativeCommons[cc].icon"
+          class="footer-cc-icon"
+        >
+      </a>
+    </div>
   </div>
 </template>
+
+<script>
+import creativeCommons from '@/util/creative-commons'
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      cc: 'by-nc',
+      creativeCommons
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.footer-container
+  width 100%
+  padding 15px 10px
+  .footer-block
+    text-align center
+    > *
+      padding 0 5px
+    .footer-cc-icon
+      height 30px
+</style>
---
+++ util/creative-commons.js
@@ -0,0 +1,13 @@
+import by from '@/assets/creative-commons/by.svg'
+import bync from '@/assets/creative-commons/by-nc.svg'
+
+export default {
+  by: {
+    url: 'https://creativecommons.org/share-your-work/licensing-types-examples/#by',
+    icon: by
+  },
+  'by-nc': {
+    url: 'https://creativecommons.org/share-your-work/licensing-types-examples/#by-nc',
+    icon: bync
+  }
+}
